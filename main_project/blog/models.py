7c714bbb59a023e8e1e1d411708017aaa3790a4c
Ana Bonturim <abonturim@gmail.com>
2024-10-16 12:05:02 -0300

    Creating Post Model

--- main_project/blog/models.py
+++ main_project/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    # TODO: adicionar o author
+    title = models.CharField(max_length=200, blank=False)
+    date = models.DateField(blank=False)
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to='images', default="default.png")
+    excerpt = models.CharField(max_length=500,blank=True)
+    content = models.TextField(blank=False)
+    
